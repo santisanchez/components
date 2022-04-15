ac66e6b3ac901fc875f09ae805a36fa61a033ece
santisanchez <santisanchez.1214@gmail.com>
2022-04-14 22:56:41 -0500

    Added label to button

--- button/index.js
+++ button/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Button({text}) {
   return (
-      <input type="button">{text}</input>
+      <>
+      <label for="component-button">{text}</label><br/>
+      <input type="button" id="component-button"/>
+      </>
   )
 }
 
