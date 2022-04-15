fdc906aac73e0e7b8d0f2efcb7afeac207031bc0
santisanchez <santisanchez.1214@gmail.com>
2022-04-14 22:59:12 -0500

    Added text input component

---
+++ text-input/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TextInput() {
+  return (<>
+      <label for="component-text-input">{text}</label><br/>
+      <input type="text" id="component-text-input"/>
+  </>)
+}
+
